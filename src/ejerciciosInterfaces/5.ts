946b653d2ab73645cefff3e976622752a31e9184
Duvier David <duvierdiegodavid.0715@gmail.com>
2024-10-27 12:59:57 -0500

    TODO EJERCICIO 5

---
+++ src/ejerciciosInterfaces/5.ts
@@ -0,0 +1,2 @@
+//* Crea una interface Database con funciones como connect, find, update, etc. Luego crea una clase MySQLDatabase e SQLiteDatabase que implementen esa interface con distintas funcionalidades.
+
